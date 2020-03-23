37bcf28b0eef7ef5aa823112c02e762f034c9a22
LindaOuer <linda.ouerfelli@gmail.com>
2020-03-23 23:10:09 +0100

    Volunteer

--- src/Controller/VoluntaryController.php
+++ src/Controller/VoluntaryController.php
@@ -2,105 +2,105 @@
 
 namespace App\Controller;
 
-use App\Entity\Voluntary;
-use App\Form\VoluntaryType;
-use App\Repository\VoluntaryRepository;
+use App\Entity\Volunteer;
+use App\Form\VolunteerType;
+use App\Repository\VolunteerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/admin/voluntary")
+ * @Route("/admin/Volunteer")
  */
-class VoluntaryController extends AbstractController
+class VolunteerController extends AbstractController
 {
     /**
-     * @Route("/", name="voluntary_index", methods={"GET"})
+     * @Route("/", name="Volunteer_index", methods={"GET"})
      */
-    public function index(VoluntaryRepository $voluntaryRepository): Response
+    public function index(VolunteerRepository $VolunteerRepository): Response
     {
-        return $this->render('voluntary/index.html.twig', [
-            'voluntaries' => $voluntaryRepository->findAll(),
+        return $this->render('Volunteer/index.html.twig', [
+            'voluntaries' => $VolunteerRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/new", name="voluntary_new", methods={"GET","POST"})
+     * @Route("/new", name="Volunteer_new", methods={"GET","POST"})
      */
     public function new(Request $request): Response
     {
-        $voluntary = new Voluntary();
-        $form = $this->createForm(VoluntaryType::class, $voluntary);
+        $Volunteer = new Volunteer();
+        $form = $this->createForm(VolunteerType::class, $Volunteer);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
-            $file = $voluntary->getPicture();
+            $file = $Volunteer->getPicture();
             // $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
             $fileName = md5(uniqid()).'.'.$file->guessExtension();
             // moves the file to the directory where brochures are stored
             $file->move($this->getParameter('pictures_directory'), $fileName);
-            $voluntary->setPicture($fileName);
-            $entityManager->persist($voluntary);
+            $Volunteer->setPicture($fileName);
+            $entityManager->persist($Volunteer);
             $entityManager->flush();
-            return $this->redirectToRoute('voluntary_index');
+            return $this->redirectToRoute('Volunteer_index');
         }
 
-        return $this->render('voluntary/new.html.twig', [
-            'voluntary' => $voluntary,
+        return $this->render('Volunteer/new.html.twig', [
+            'Volunteer' => $Volunteer,
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/{id}", name="voluntary_show", methods={"GET"})
+     * @Route("/{id}", name="Volunteer_show", methods={"GET"})
      */
-    public function show(Voluntary $voluntary): Response
+    public function show(Volunteer $Volunteer): Response
     {
-        return $this->render('voluntary/show.html.twig', [
-            'voluntary' => $voluntary,
+        return $this->render('Volunteer/show.html.twig', [
+            'Volunteer' => $Volunteer,
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="voluntary_edit", methods={"GET","POST"})
+     * @Route("/{id}/edit", name="Volunteer_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, Voluntary $voluntary): Response
+    public function edit(Request $request, Volunteer $Volunteer): Response
     {
-        $form = $this->createForm(VoluntaryType::class, $voluntary);
+        $form = $this->createForm(VolunteerType::class, $Volunteer);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $file = $voluntary->getPicture();
+            $file = $Volunteer->getPicture();
             // $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
             $fileName = md5(uniqid()).'.'.$file->guessExtension();
             // moves the file to the directory where brochures are stored
             $file->move($this->getParameter('pictures_directory'), $fileName);
-            $voluntary->setPicture($fileName);
+            $Volunteer->setPicture($fileName);
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('voluntary_index');
+            return $this->redirectToRoute('Volunteer_index');
         }
 
-        return $this->render('voluntary/edit.html.twig', [
-            'voluntary' => $voluntary,
+        return $this->render('Volunteer/edit.html.twig', [
+            'Volunteer' => $Volunteer,
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/{id}", name="voluntary_delete", methods={"DELETE"})
+     * @Route("/{id}", name="Volunteer_delete", methods={"DELETE"})
      */
-    public function delete(Request $request, Voluntary $voluntary): Response
+    public function delete(Request $request, Volunteer $Volunteer): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$voluntary->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete'.$Volunteer->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($voluntary);
+            $entityManager->remove($Volunteer);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('voluntary_index');
+        return $this->redirectToRoute('Volunteer_index');
     }
 
     /**
--- src/Form/VolunteerType.php
+++ src/Form/VolunteerType.php
@@ -2,12 +2,12 @@
 
 namespace App\Form;
 
-use App\Entity\Voluntary;
+use App\Entity\Volunteer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class VoluntaryType extends AbstractType
+class VolunteerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -24,7 +24,7 @@ class VoluntaryType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Voluntary::class,
+            'data_class' => Volunteer::class,
         ]);
     }
 }
