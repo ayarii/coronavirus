49d00b4d17a7a2a1b651bb473026e1da37ee1175
asma <asma.ayari.dev@gmail.com>
2020-03-23 21:08:49 +0100

    modify entity information

--- src/Entity/Information.php
+++ src/Entity/Information.php
@@ -36,6 +36,61 @@ class Information
      */
     private $cin;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $fever;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $cough;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $lossOfTaste;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $soreThroat;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $tired;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $diarrhea;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+   private $problemFeeding;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $pregnant;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $treatment;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -88,4 +143,181 @@ class Information
 
         return $this;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender): void
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFever()
+    {
+        return $this->fever;
+    }
+
+    /**
+     * @param mixed $fever
+     */
+    public function setFever($fever): void
+    {
+        $this->fever = $fever;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCough()
+    {
+        return $this->cough;
+    }
+
+    /**
+     * @param mixed $cough
+     */
+    public function setCough($cough): void
+    {
+        $this->cough = $cough;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLossOfTaste()
+    {
+        return $this->lossOfTaste;
+    }
+
+    /**
+     * @param mixed $lossOfTaste
+     */
+    public function setLossOfTaste($lossOfTaste): void
+    {
+        $this->lossOfTaste = $lossOfTaste;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSoreThroat()
+    {
+        return $this->soreThroat;
+    }
+
+    /**
+     * @param mixed $soreThroat
+     */
+    public function setSoreThroat($soreThroat): void
+    {
+        $this->soreThroat = $soreThroat;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTired()
+    {
+        return $this->tired;
+    }
+
+    /**
+     * @param mixed $tired
+     */
+    public function setTired($tired): void
+    {
+        $this->tired = $tired;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDiarrhea()
+    {
+        return $this->diarrhea;
+    }
+
+    /**
+     * @param mixed $diarrhea
+     */
+    public function setDiarrhea($diarrhea): void
+    {
+        $this->diarrhea = $diarrhea;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProblemFeeding()
+    {
+        return $this->problemFeeding;
+    }
+
+    /**
+     * @param mixed $problemFeeding
+     */
+    public function setProblemFeeding($problemFeeding): void
+    {
+        $this->problemFeeding = $problemFeeding;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPregnant()
+    {
+        return $this->pregnant;
+    }
+
+    /**
+     * @param mixed $pregnant
+     */
+    public function setPregnant($pregnant): void
+    {
+        $this->pregnant = $pregnant;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTreatment()
+    {
+        return $this->treatment;
+    }
+
+    /**
+     * @param mixed $treatment
+     */
+    public function setTreatment($treatment): void
+    {
+        $this->treatment = $treatment;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date): void
+    {
+        $this->date = $date;
+    }
+
 }
