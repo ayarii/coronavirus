5c89e5ddd5648de659b96bb77889a840f5803605
Radhouene Massoudi <massoudiradhouene@gmail.com>
2020-03-26 20:10:50 +0100

    authentification

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(AuthenticationUtils $helper): Response
+    {
+        return $this->render('Security/login.html.twig', [
+            'last_username' => $helper->getLastUsername(),
+            'error' => $helper->getLastAuthenticationError(),
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached!');
+    }
+}
