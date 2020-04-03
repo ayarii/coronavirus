ea34ba9b41f6bdb181db6c5b73a4f81aff90b80a
asma <asma.ayari.dev@gmail.com>
2020-04-03 15:54:22 +0200

    add statistic to dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,10 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Volunteer;
 use App\Repository\VolunteerRepository;
-use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
@@ -17,9 +15,15 @@ class DashboardController extends AbstractController
      */
     public function dashboard()
     {
+        $volunteers= $this->getDoctrine()->getRepository('App:Volunteer');
+        $url = "https://api.kawalcorona.com/";
+        $client = HttpClient::create();
+        $response = $client->request('GET', $url);
+        $results = json_decode($response->getContent(),true);
+
         return $this->render('dashboard/dashboard.html.twig', [
-            'controller_name' => 'HomeController',
+            'results'=>$results,
+            'voluntaries' => $volunteers
         ]);
     }
-
 }
